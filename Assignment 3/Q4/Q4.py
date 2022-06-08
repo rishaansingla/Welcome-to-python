2212381cc0051fe8e3e39a3ab7903cad6da32af2
rishaansingla <103490065+rishaansingla@users.noreply.github.com>
2022-06-08 15:46:15 +0530

    Create Q4.py

---
+++ Assignment 3/Q4/Q4.py
@@ -0,0 +1,18 @@
+for i in range(5):
+    print(i, end=" ")
+print()
+
+for i in range(3,10):
+    print(i, end=" ")
+print()
+
+for i in range(4,13,3):
+    print(i, end=" ")
+print()
+
+for i in range(15,5,-2):
+    print(i, end=" ")
+print()
+
+for i in range(5,3):
+    print(i, end=" ")
