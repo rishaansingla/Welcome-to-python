5e8ce0655f8c238126799e0b30b8927996830ded
rishaansingla <103490065+rishaansingla@users.noreply.github.com>
2022-06-08 15:45:33 +0530

    Create Q3.py

---
+++ Assignment 3/Q3/Q3.py
@@ -0,0 +1,25 @@
+import math
+
+#Define variables to be used
+n = 5
+r = 5
+α = 5
+β = 5
+y2 = 10
+y1 = 5
+x2 = 20
+x1 = 5
+
+# The required Expressions
+exp1 = (3+4) * (5)
+exp2 = ((n)*(n-1)) / 2
+exp3 = 4*(math.pi) * (math.pow(r,2))
+exp4 = math.sqrt(r*(math.pow(math.cos(α), 2)) + r*(math.pow(math.sin(β), 2)))
+exp5 = (y2 - y1)/(x2 - x1)
+
+#Printing the output of Expressions
+print(exp1)
+print(exp2)
+print(exp3)
+print(exp4)
+print(exp5)
