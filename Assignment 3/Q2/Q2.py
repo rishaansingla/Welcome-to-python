87c725fd2dd8e639cbb27207a576fdef3502636e
rishaansingla <103490065+rishaansingla@users.noreply.github.com>
2022-06-08 15:44:24 +0530

    Create Q2.py

---
+++ Assignment 3/Q2/Q2.py
@@ -0,0 +1,17 @@
+num = float(input('Enter first number: '))
+sign = input('Enter sign(+, -, *, / or q for quit): ')
+
+while (sign!='q'):
+    if sign=='+':
+        num = num + float(input('Next number: '))
+    elif sign == '-':
+        num = num - float(input('Next number: '))
+    elif sign == '*':
+        num = num * float(input('Next number: '))
+    elif sign == '/':
+        num = num / float(input('Next number: '))
+    else:
+        print('Invalid operator, try again')
+    sign = input('Enter next sign(+, -, *, / or q for quit): ')
+
+print(num)
